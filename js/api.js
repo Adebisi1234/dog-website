aaff052184e30c16fe01b726d0a7e54f74dcfed1
Adebisi Tobiloba <oluwatobilobaadebisi6@gmail.com>
2022-07-10 19:13:04 +0100

    done but it isnt mobile responsive

--- js/api.js
+++ js/api.js
@@ -1,32 +1,32 @@
-let interval = setInterval(api, 5000)
+//let interval = setInterval(api, 5000)
 
 let count = 0
 const lab = document.querySelector('#labrador')
 const german = document.querySelector('#german')
-const poodle = document.querySelector('#poodle')
+const poodle = document.querySelector('#poodles')
 const home = document.querySelector('.home-intro')
 const beagle = document.querySelector('#beagle')
 const frenchie = document.querySelector('#frenchie')
 const dog = [lab, german, poodle, beagle, frenchie]
-const dogUrl = ['labrador','germanshepherd','poodle/standard','beagle','french/bulldog']
+const dogUrl = ['labrador','germanshepherd','poodle','beagle','bulldog/french']
 
 function api() {
     count++
-    if(count === 3){
-        clearInterval(interval)
-        interval = null
-    }
+    
     for(let i = 0; i < dogUrl.length; i++){
-        fetch(`https://dog.ceo/api/breed/${dogUrl[i]}/image/random`)
+        fetch(`https://dog.ceo/api/breed/${dogUrl[i]}/images/random`)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            dog[i].src = data.message
+            dog[i].src = `${data.message}`
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
     }  
-    
+    if(count === 3){
+        clearInterval(interval)
+        interval = null
+    }
 }
 
 
